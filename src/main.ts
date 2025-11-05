8ebe2f1a837414d802d0b13096cf4540e81a5512
NoahBaron05 <156266104+NoahBaron05@users.noreply.github.com>
2025-11-05 21:04:09 +0000

    Added map with player location (Steps 3 and 4 Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 // @deno-types="npm:@types/leaflet"
+import leaflet from "leaflet";
 
 // Style sheets
 import "leaflet/dist/leaflet.css"; // supporting style for Leaflet
@@ -8,3 +9,31 @@ import "./style.css"; // student-controlled page style
 import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
 
 // Import our luck function
+
+const mapDiv = document.createElement("div");
+mapDiv.id = "map";
+document.body.append(mapDiv);
+
+const SPAWN_POINT = leaflet.latLng(57.476538, -4.225123);
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+
+const map = leaflet.map(mapDiv, {
+  center: SPAWN_POINT,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+const playerLocation = leaflet.marker(SPAWN_POINT);
+playerLocation.addTo(map);
